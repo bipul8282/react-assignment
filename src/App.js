fe7e98b88f78aeccdbf658acf8335a5dfb94ab74
unknown <kb84101@gmail.com>
2022-12-14 08:45:43 +0530

    assignment 1 problem 1 sol

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  <Page/>
+    </div>
+  );
+}
+
+export default App;
+
+
+function Page(){
+
+  return (
+    <div>
+    
+    <ol>
+    <li>React is very popular</li>
+    <li>react is fun</li>
+    <li>functionup</li>
+    <li>hey therer</li>
+
+    </ol>
+
+    </div>
+  )
+}
